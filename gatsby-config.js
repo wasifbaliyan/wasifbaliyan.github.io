b90308810dfa29b32733d8a8b38615401710c992
Wasif Baliyan <w.baliyan@gmail.com>
2020-06-01 23:34:24 +0530

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wasif Baliyan | Web Developer`,
+    description: `Web Developer Portfolio of Wasif Baliyan`,
+    author: `Wasif Baliyan`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
