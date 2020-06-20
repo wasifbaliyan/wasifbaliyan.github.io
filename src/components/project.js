bc33b6399f0124a8e3afafaedc1866a75d80f191
Wasif Baliyan <w.baliyan@gmail.com>
2020-06-20 12:03:50 +0530

    added the linear gradient to project back image

--- src/components/project.js
+++ src/components/project.js
@@ -2,25 +2,38 @@ import React from "react"
 
 import style from "./project.module.scss"
 
-const Project = ({ project }) => (
-  <div className={style.project}>
-    <div className={style.project__img}>&nbsp;</div>
-    <h3 className={style.project__title}>{project.title}</h3>
-    <div className={style.project__content}>
-      {project.tags.map(tag => (
-        <span className={style.project__tag}>{tag}</span>
-      ))}
-      <p className={style.project__text}>{project.desc}</p>
-      <div className={style.btngroup}>
-        <a className={style.btngroup__btn} href={project.github}>
-          Github
-        </a>
-        <a className={style.btngroup__btn} href={project.live}>
-          Live
-        </a>
+const Project = ({ project }) => {
+  return (
+    <div className={style.project}>
+      <div
+        style={{
+          backgroundImage: `linear-gradient(
+          to right bottom,
+          #2998ff,
+          #5643fa
+        ) , url(${project.imgUrl})`,
+        }}
+        className={style.project__img}
+      >
+        &nbsp;
+      </div>
+      <h3 className={style.project__title}>{project.title}</h3>
+      <div className={style.project__content}>
+        {project.tags.map(tag => (
+          <span className={style.project__tag}>{tag}</span>
+        ))}
+        <p className={style.project__text}>{project.desc}</p>
+        <div className={style.btngroup}>
+          <a className={style.btngroup__btn} href={project.github}>
+            Github
+          </a>
+          <a className={style.btngroup__btn} href={project.live}>
+            Live
+          </a>
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Project
