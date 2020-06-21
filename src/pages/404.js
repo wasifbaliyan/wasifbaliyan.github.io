af29d1060e12ae980207d3332cd27e46866dfa47
Wasif Baliyan <w.baliyan@gmail.com>
2020-06-21 15:17:24 +0530

    update 404 page styles

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,7 +9,7 @@ import style from "./404.module.scss"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1 className={style.notfound}>NOT FOUND</h1>
+    <h1 className={style.notfound}>Not Found</h1>
     <p className={style.notfound__text}>
       You just hit a route that doesn&#39;t exist... the sadness.
     </p>
